fc9cf4ade615c574aa8889f0454ae6cb87531f30
Daniel Thom <daniel.thom@nrel.gov>
2022-09-21 08:15:17 -0600

    Use PreventUpdate

--- src/system_viewer_app.jl
+++ src/system_viewer_app.jl
@@ -10,7 +10,6 @@ using PowerSystemManager
 
 const IS = InfrastructureSystems
 const PSY = PowerSystems
-const PSM = PowerSystemManager
 const DEFAULT_UNITS = "unknown"
 
 include("utils.jl")
@@ -20,7 +19,8 @@ mutable struct SystemData
     system::Union{Nothing, System}
 end
 
-get_system(session::Session{SystemData}) = PSM.get_data(session).system
+SystemData() = SystemData(nothing)
+get_system(data::SystemData) = data.system
 
 function get_component_table(sys, component_type)
     return make_component_table(getproperty(PowerSystems, Symbol(component_type)), sys)
@@ -161,21 +161,21 @@ component_tab = dcc_tab(
     ],
 )
 
-function serve_layout()
-    session_id = string(UUIDs.uuid4())
-    add_session!(g_store, Session(session_id, SystemData(nothing)))
-    return html_div([
-        dcc_store(data=session_id, id="session_id"),
+# Note: This is only setup to support one worker. We would need to implement a backend
+# process that manages a store and provides responses to each Dash worker. The code in this
+# file would not be able to use any PSY functionality. There would have to be API calls
+# to retrieve the data from the backend process.
+g_data = SystemData()
+get_system() = get_system(g_data)
+app = dash()
+app.layout = html_div() do
+    html_div([
         dcc_tabs([
             dcc_tab(label="System", children=[system_tab]),
             dcc_tab(label="Time Series", children=[component_tab]),
         ]),
     ])
 end
-
-g_store = Store{SystemData}()
-app = dash()
-app.layout = serve_layout
 
 callback!(
     app,
@@ -185,15 +185,12 @@ callback!(
     Output("component_type_radio", "options"),
     Input("loading_system", "children"),
     Input("load_button", "n_clicks"),
-    Input("session_id", "data"),
     State("system_text", "value"),
     State("load_description", "value"),
-) do loading_system, n_clicks, session_id, system_path, load_description
-    n_clicks <= 0 && return loading_system, "None", DEFAULT_UNITS, []
-    session = get_session(g_store, session_id)
+) do loading_system, n_clicks, system_path, load_description
+    n_clicks <= 0 && throw(PreventUpdate())
     system = System(system_path, time_series_read_only=true)
-    set_data!(session, SystemData(system))
-
+    g_data.system = system
     return (
         loading_system,
         "$system_path\n$(summary(system))",
@@ -206,11 +203,9 @@ callback!(
     app,
     Output("component_type_radio", "value"),
     Input("component_type_radio", "options"),
-    Input("session_id", "data"),
-) do available_options, session_id
-    session = get_session(g_store, session_id)
-    isnothing(get_system(session)) && return ""
-    return get_default_component_type(get_system(session))
+) do available_options
+    isnothing(get_system()) && throw(PreventUpdate())
+    return get_default_component_type(get_system())
 end
 
 callback!(
@@ -219,11 +214,10 @@ callback!(
     Output("num_components_text", "value"),
     Input("units_radio", "value"),
     Input("component_type_radio", "value"),
-    Input("session_id", "data"),
-) do units, component_type, session_id
-    session = get_session(g_store, session_id)
-    system = get_system(session)
-    isnothing(system) && return nothing, "0"
+) do units, component_type
+    (units == "" || component_type == "") && throw(PreventUpdate())
+    system = get_system()
+    @assert !isnothing(system)
     @assert units != DEFAULT_UNITS
     if get_system_units(system) != units
         set_units_base_system!(system, units)
@@ -239,12 +233,11 @@ callback!(
     Output("deterministic_datatable_container", "children"),
     Input("components_datatable", "derived_viewport_selected_rows"),
     Input("components_datatable", "derived_viewport_data"),
-    Input("session_id", "data"),
     State("component_type_radio", "value"),
-) do row_indexes, row_data, session_id, component_type
-    (isnothing(row_indexes) || isempty(row_indexes) || isnothing(row_indexes[1])) &&
-        return "", [], []
-    session = get_session(g_store, session_id)
+) do row_indexes, row_data, component_type
+    if (isnothing(row_indexes) || isempty(row_indexes) || isnothing(row_indexes[1]))
+        throw(PreventUpdate())
+    end
     static_time_series = []
     deterministic_time_series = []
     for i in row_indexes
@@ -252,7 +245,7 @@ callback!(
         row = row_data[row_index]
         component_name = row["name"]
         type = getproperty(PowerSystems, Symbol(component_type))
-        component = get_component(type, get_system(session), component_name)
+        component = get_component(type, get_system(), component_name)
         component_text = "$component_type $component_name"
 
         if has_time_series(component)
@@ -337,14 +330,9 @@ callback!(
     Input("plot_sts_button", "n_clicks"),
     Input("sts_datatable", "derived_viewport_selected_rows"),
     Input("sts_datatable", "derived_viewport_data"),
-    Input("session_id", "data"),
     State("selected_component_type", "value"),
-) do n_clicks, row_indexes, row_data, session_id, component_type
-    ctx = callback_context()
-    if n_clicks < 1 || ctx.triggered[1].prop_id != "plot_sts_button.n_clicks"
-        return Plot()
-    end
-    session = get_session(g_store, session_id)
+) do n_clicks, row_indexes, row_data, component_type
+    n_clicks < 1 && throw(PreventUpdate())
     traces = []
     for i in row_indexes
         row_index = i + 1  # julia is 1-based
@@ -353,7 +341,7 @@ callback!(
         ts_type = getproperty(PowerSystems, Symbol(row["type"]))
         c_name = row["component_name"]
         c_type = getproperty(PowerSystems, Symbol(component_type))
-        component = get_component(c_type, get_system(session), c_name)
+        component = get_component(c_type, get_system(), c_name)
         ta = get_time_series_array(ts_type, component, ts_name)
         trace = scatter(;
             x=TimeSeries.timestamp(ta),
@@ -371,10 +359,6 @@ callback!(
     return plot([x for x in traces], layout)
 end
 
-# Note: This is only setup to support one worker. We would need to implement a backend
-# process that manages a store and provides responses to each Dash worker. The code in this
-# file would not be able to use any PSY functionality. There would have to be API calls
-# to retrieve the data from the backend process.
 if !isnothing(get(ENV, "SIIP_DEBUG", nothing))
     run_server(app, "0.0.0.0", debug=true, dev_tools_hot_reload=true)
 else
